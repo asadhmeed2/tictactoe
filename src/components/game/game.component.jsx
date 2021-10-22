62b77e8ed406cda48b46587c1c352923be464eb9
newasad <asadhmeed2@gmail.com>
2021-10-23 01:25:10 +0300

    finished game logic

--- src/components/game/game.component.jsx
+++ src/components/game/game.component.jsx
@@ -15,17 +15,69 @@ const Game = () => {
     "",
   ]);
   const [playerState, setPlayerState] = useState("x");
+  const [timeState, setTimeState] = useState([]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [thereIsWinner, setThereIsWinner] = useState(false);
 
-  const onBtnClick =(e)=>{
-    let newBordState=[...boardState];
-    console.log("player is :",playerState);
-    newBordState[e.target.id] = playerState;
-    setPlayerState(playerState==="x"?"o":"x");
-    setBoardState(newBordState);
-  }
+  const checkWinner = (newBoardState) => {
+    const winingStrick = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [6, 4, 2],
+      [0, 4, 8],
+    ];
+    let thereIsWin = winingStrick.filter((el) => {
+      let [a, b, c] = el;
+      return (
+        newBoardState[a] &&
+        newBoardState[a] === newBoardState[b] &&
+        newBoardState[a] === newBoardState[c]
+      );
+    });
+    console.log(thereIsWin);
+    if (thereIsWin.length) {
+      return thereIsWin;
+    }
+    return null;
+  };
+  const onBtnClick = (e) => {
+    if (!thereIsWinner) {
+      let newBoardState = [...boardState];
+      if (!newBoardState[e.target.id]) {
+        newBoardState[e.target.id] = playerState;
+        let currentStep = stepNumber + 1;
+        setStepNumber(currentStep);
+        setPlayerState(playerState === "x" ? "o" : "x");
+        setBoardState(newBoardState);
+        setTimeState([...timeState, newBoardState]);
+        let winner = checkWinner(newBoardState);
+        // console.log(winner);
+        if (winner) {
+          setThereIsWinner(true);
+        }
+        if (currentStep === 9) {
+          console.log("drow");
+        }
+      }
+    }
+  };
   return (
     <div className="game">
       <Board boardState={boardState} onClick={onBtnClick} />
+      <div className="footer">
+        {timeState.map((el, i) => {
+          return (
+            <div className="timeStep" key={i} id={i}>
+              {i + 1}
+            </div>
+          );
+        })}
+        
+      </div>
     </div>
   );
 };
